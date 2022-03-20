cf501c6d7b4853f4e7b4637e2054dbbc6a4931e0
Terrance <terranceli.terbear@gmail.com>
2022-03-20 13:21:35 -0700

    Added new min-fsl configuration option for maxmemory-policy; added 10MB max memory to config

--- src/config.c
+++ src/config.c
@@ -60,6 +60,7 @@ configEnum maxmemory_policy_enum[] = {
     {"allkeys-lfu",MAXMEMORY_ALLKEYS_LFU},
     {"allkeys-random",MAXMEMORY_ALLKEYS_RANDOM},
     {"noeviction",MAXMEMORY_NO_EVICTION},
+    {"min-fsl", MAXMEMORY_MIN_FSL},
     {NULL, 0}
 };
 
