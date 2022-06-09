daac06b733c36447919f7183ad8ff260f40659c1
Audrey Cheng <audreycc@fb.com>
2022-06-08 23:30:00 -0700

    exponential groups

--- src/t_string.c
+++ src/t_string.c
@@ -622,7 +622,7 @@ void mgetCommand(client *c) {
       // naive grouping
       int even = (c->argc-1)/2 + 1;
       int pow_set_size = pow(2, c->argc-1);
-      double scores2[pow_set_size*pow_set_size];
+      double scores2[pow_set_size];
     //   for (int i = 0; i < pow_set_size; i++) {
       for(int counter = 0; counter < pow_set_size; counter++) {
         double score = 0.0;
